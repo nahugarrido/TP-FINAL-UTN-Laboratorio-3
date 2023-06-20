1e8cdc02fc9dd9ce225b7c1542ba76740732962e
Nahuel <68355186+nahugarrido@users.noreply.github.com>
2023-06-20 19:53:26 -0300

    documentacion

--- src/genericas/GenericaMap.java
+++ src/genericas/GenericaMap.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Clase generica que utiliza un HashMap internamente
+ * @param <K> Key
+ * @param <T> Valor
+ */
 public class GenericaMap<K,T> implements Serializable {
 
     HashMap<K, T> mapGenerico;
@@ -12,23 +17,44 @@ public class GenericaMap<K,T> implements Serializable {
         this.mapGenerico = new HashMap<>();
     }
 
+    /**
+     * Agregar un elemento nuevo al map
+     * @param key clave
+     * @param elemento valor
+     */
     public  void agregarElemento(K key, T elemento) {
         mapGenerico.put(key, elemento);
     }
 
-
+    /**
+     * Eliminar un elemento del map
+     * @param key clave del elemento
+     */
     public  void eliminarElemento(K key) {
         mapGenerico.remove(key);
     }
 
+    /**
+     * Obtener el valor asociado a una clave
+     * @param key clave
+     * @return
+     */
     public T obtenerValor(K key) {
         return mapGenerico.get(key);
     }
 
+    /**
+     * Contar cantidad de elementos en el map
+     * @return cantidad de elementos
+     */
     public int contarElementos() {
         return mapGenerico.size();
     }
 
+    /**
+     * Muestra los elementos presentes en el map
+     * @return texto con los elementos
+     */
     public String listarElementos() {
         StringBuilder texto  = new StringBuilder();
         for(Map.Entry<K,T> elemento : mapGenerico.entrySet()) {
--- src/interfaces/I_ToJson.java
+++ src/interfaces/I_ToJson.java
@@ -3,6 +3,9 @@ package interfaces;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Interfaz para crear instancias de clases a partir de un json
+ */
 public interface I_ToJson {
     void fromJSON(JSONObject jsonObject) throws JSONException;
 }
