23a126859b503301e4428474538c69a1315ba973
Oliva-Sebastian <sebastianoliva@estudiante.mdp.utn.edu.ar>
2023-06-20 21:51:07 -0300

    documentacion
    documentacion

--- src/genericas/ListaLotes.java
+++ src/genericas/ListaLotes.java
@@ -6,29 +6,48 @@ import modelos.granja.Lote;
 
 import java.io.Serializable;
 
+/**
+ * Esta clase representa una lista genérica de lotes de huevos.
+ */
 public class ListaLotes extends GenericaList<Lote> implements Serializable {
 
+    /**
+     * Obtiene una lista de lotes pertenecientes a una granja específica
+     *
+     * @param idGranja ID de la granja
+     * @return Lista de lotes pertenecientes a la granja.
+     * @throws NoHayLotesException no hay lotes disponibles para la granja.
+     */
     public GenericaList<Lote> obtenerLotes(int idGranja) throws NoHayLotesException {
         GenericaList<Lote> listaAuxiliar = new GenericaList<>();
-        for(Lote elemento : listaGenerica) {
-            if(elemento.getIdGranja() == idGranja) {
+        for (Lote elemento : listaGenerica) {
+            if (elemento.getIdGranja() == idGranja) {
                 listaAuxiliar.agregarElemento(elemento);
             }
         }
 
-        if(listaAuxiliar.contarElementos() == 0) {
+        if (listaAuxiliar.contarElementos() == 0) {
             throw new NoHayLotesException("", listaAuxiliar.contarElementos());
         }
 
         return listaAuxiliar;
     }
 
+
+    /**
+     * Vende los lotes de huevos de una granja y devuelve los ingresos totales.
+     *
+     * @param idGranja  ID de la granja.
+     * @param cotizacionHuevos Cotización actual de los huevos.
+     * @return Ingresos totales por la venta de los lotes de huevos.
+     * @throws NoHayLotesException Si no hay lotes disponibles para la granja.
+     */
     /// HAY QUE VER LA COTIZACION DEL DIA.
     public double venderLotes(int idGranja, double cotizacionHuevos) throws NoHayLotesException {
         double ingresosTotales = 0;
 
-        for(Lote item : listaGenerica) {
-            if(item.getIdGranja() == idGranja && !item.isEstaVendido()) {
+        for (Lote item : listaGenerica) {
+            if (item.getIdGranja() == idGranja && !item.isEstaVendido()) {
                 double gananciaBlancos = item.obtenerCantidad(EnumColor.BLANCO) * (cotizacionHuevos + 5);
                 double gananciaCrema = item.obtenerCantidad(EnumColor.CREMA) * (cotizacionHuevos - 5);
                 double gananciaMarrones = item.obtenerCantidad(EnumColor.MEDIO_CLARO) * (cotizacionHuevos);
@@ -39,7 +58,7 @@ public class ListaLotes extends GenericaList<Lote> implements Serializable {
                 item.setEstaVendido(true);
             }
         }
-        if(ingresosTotales == 0) {
+        if (ingresosTotales == 0) {
             throw new NoHayLotesException("", 0);
         }
         return ingresosTotales;
--- src/genericas/ListaUsuarios.java
+++ src/genericas/ListaUsuarios.java
@@ -6,8 +6,19 @@ import modelos.usuarios.Usuario;
 
 import java.io.Serializable;
 
+/**
+ * Esta clase representa una lista genérica de usuarios.
+ */
 public class ListaUsuarios extends GenericaList<Usuario> implements Serializable {
 
+    /**
+     * Verifica los datos de acceso de un usuario.
+     *
+     * @param usuario Nombre de usuario.
+     * @param clave   Clave de acceso.
+     * @return El usuario si los datos son válidos.
+     * @throws UsuarioNoValidoException Si los datos de acceso no son válidos.
+     */
 
     /// hay que ver si no hay que crear otra clase generica o si se puede dejar este metodo aca
     public Usuario verificarDatosAcceso(String usuario, String clave) throws UsuarioNoValidoException {
@@ -19,6 +30,13 @@ public class ListaUsuarios extends GenericaList<Usuario> implements Serializable
         throw new UsuarioNoValidoException("");
     }
 
+
+    /**
+     * Verifica si un usuario ya existe en la lista.
+     *
+     * @param usuario Nombre de usuario a verificar.
+     * @throws UsuarioYaExistenteException Si el usuario ya existe en la lista.
+     */
     public void verificarSiExisteUsuario(String usuario) throws UsuarioYaExistenteException {
         for (Usuario elemento : listaGenerica) {
             if (elemento.getUsuario().equals(usuario)) {
@@ -27,6 +45,12 @@ public class ListaUsuarios extends GenericaList<Usuario> implements Serializable
         }
     }
 
+    /**
+     * Actualiza el ID de granja de un usuario.
+     *
+     * @param idUsuario ID del usuario.
+     * @param idGranja  Nuevo ID de granja.
+     */
     public void actualizarIdGranja(int idUsuario, int idGranja) {
         for (Usuario elemento : listaGenerica) {
             if (elemento.getId() == idUsuario) {
